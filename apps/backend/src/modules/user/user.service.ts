ff1446772517c47c36590415115caf74b8c43786
itdongquoctien <itdongquoctien@gmail.com>
2026-02-15 21:21:55 +0700

    Update bcrypt dependency in user service for improved security

--- apps/backend/src/modules/user/user.service.ts
+++ apps/backend/src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException, ConflictException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UserEntity } from './user.entity';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AdminCreateUserDto, AdminUpdateUserDto } from './dto/admin-user.dto';
